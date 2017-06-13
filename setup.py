01aca1cee22326212beb71fe4e4cb3657c8cdb44
Ali Ghahraei Figueroa <aligf94@gmail.com>
2017-06-13 18:37:35 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='just-start',
+    version='0.1.0',
+    description='Just Start is an ncurses wrapper for Task Warrior with pomodoro support',
+    author='Ali Ghahraei Figueroa',
+    author_email='aligf94@gmail.com',
+    url='https://github.com/AliGhahraei/just-start',
+    py_modules=['just-start'],
+)
