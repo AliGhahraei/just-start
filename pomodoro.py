4676c36eecb5ceadc8be0540f44b508ed7f194e2
Ali Ghahraei <aligf94@gmail.com>
2017-12-07 08:53:31 -0600

    Avoid trying to cancel a non-running timer (possibly NoneType)

--- pomodoro.py
+++ pomodoro.py
@@ -54,4 +54,5 @@ class PomodoroTimer():
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.timer.cancel()
+        if(self.is_running):
+            self.timer.cancel()
