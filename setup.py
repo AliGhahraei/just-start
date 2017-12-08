d7a4fc3d2b9bcf50e845c3bd07225a7d89979856
Ali Ghahraei <aligf94@gmail.com>
2017-12-07 18:26:03 -0600

    Respec PEP8

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(
     name='just-start',
     version='0.1.0',
-    description='Just Start is an ncurses wrapper for Task Warrior with pomodoro support',
+    description=('Just Start is an ncurses wrapper for Task Warrior with '
+                 'pomodoro support'),
     author='Ali Ghahraei Figueroa',
     author_email='aligf94@gmail.com',
     url='https://github.com/AliGhahraei/just-start',
