df40f62956baeb216db433cac9c347bd0ca73828
Ali Ghahraei <aligf94@gmail.com>
2018-02-02 21:22:38 -0600

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='just-start',
-    version='0.1.0',
+    version='0.2.0',
     description=('Just Start is an ncurses wrapper for Task Warrior with '
                  'pomodoro support'),
     author='Ali Ghahraei Figueroa',
