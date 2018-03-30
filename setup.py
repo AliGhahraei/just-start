815f1476d7fae6a042bd9772ef47258993d68e6c
AliGhahraei <aligf94@gmail.com>
2018-03-30 13:18:47 -0600

    Remove unnecessary parenthesis

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='just-start',
     version='0.2.0',
-    description=('Just Start is a wrapper for Task Warrior with pomodoro'
-                 ' support'),
+    description='Just Start is a wrapper for Task Warrior with pomodoro'
+                ' support',
     author='Ali Ghahraei Figueroa',
     author_email='aligf94@gmail.com',
     url='https://github.com/AliGhahraei/just-start',
