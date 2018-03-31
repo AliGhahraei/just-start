2e0f59d8a7422cd821ea23ca225f8c2b8bcefef5
AliGhahraei <aligf94@gmail.com>
2018-03-31 12:14:09 -0600

    Add __all__ to __init__

--- just_start/__init__.py
+++ just_start/__init__.py
@@ -1,10 +1,17 @@
-# noinspection PyUnresolvedReferences
 from ._just_start import (
     client, prompt_and_exec_action, init, read_serialized_data,
     refresh_tasks_and_sync, handle_sigterm, quit_gracefully, Action, KEY_ACTIONS
 )
-# noinspection PyUnresolvedReferences
+
 from .log import logger
-# noinspection PyUnresolvedReferences
+
 from .utils import (JustStartError, TaskWarriorError, ActionError,
                     UserInputError, PromptKeyboardInterrupt)
+
+
+__all__ = [
+    'client', 'prompt_and_exec_action', 'init', 'read_serialized_data',
+    'refresh_tasks_and_sync', 'handle_sigterm', 'quit_gracefully', 'Action',
+    'KEY_ACTIONS', 'ActionError', 'JustStartError', 'TaskWarriorError',
+    'UserInputError', 'PromptKeyboardInterrupt', 'logger',
+]
