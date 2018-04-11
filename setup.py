e2672db68e1949d54bfbd415921774b9573618f3
AliGhahraei <aligf94@gmail.com>
2018-04-11 10:07:46 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='just-start',
-    version='0.4.0',
+    version='0.4.1',
     description='Just Start is a wrapper for Task Warrior with pomodoro'
                 ' support',
     author='Ali Ghahraei Figueroa',
