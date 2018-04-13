63ccfbcf8d01f4c706d4b54bca755b9e23a97914
AliGhahraei <aligf94@gmail.com>
2018-04-13 00:35:39 -0500

    Stop checking client decorator calls (too much work, almost no benefit)

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,28 +1,16 @@
-from just_start import client, Action
+from just_start import client
 
 
 # noinspection PyUnusedLocal
 def test_client_decorator_definition_and_calls(mocker, no_wifi_handling):
     @client
-    def write_status(*_):
-        functions_called[write_status] = True
+    def write_status(_):
+        pass
 
     @client
-    def write_pomodoro_status(*_):
-        functions_called[write_pomodoro_status] = True
+    def write_pomodoro_status(_):
+        pass
 
     @client
-    def on_tasks_refresh(*_):
-        functions_called[on_tasks_refresh] = True
-
-    functions_called = {function_: False for function_ in
-                        (write_status, write_pomodoro_status, on_tasks_refresh)}
-
-    with mocker.patch('just_start._just_start.refresh_tasks'):
-        Action.REFRESH_TASKS()
-
-    with mocker.patch('just_start.pomodoro.run'):
-        Action.STOP_TIMER()
-
-    for function_, called in functions_called.items():
-        assert called, f'{function_.__name__} was not called'
+    def on_tasks_refresh(_):
+        pass
