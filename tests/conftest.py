9da6517cec48733bf58cdf25a3c2fdbccfa09757
AliGhahraei <aligf94@gmail.com>
2018-04-14 14:48:27 -0500

    Mock subprocess' run instead of wifi function

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,6 @@ from unittest.mock import patch
 
 
 @fixture(scope='session', autouse=True)
-def mock_wifi_management():
-    patch('just_start._just_start.manage_wifi')
+def mock_subprocess_run():
+    patch('just_start.utils.run')
+    patch('just_start.pomodoro.run')
