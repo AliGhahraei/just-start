9675f069e674cb4a146247d87cd42ae4d1d727b4
AliGhahraei <aligf94@gmail.com>
2018-04-14 14:34:08 -0500

    Add and refactor tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@ from pytest import fixture
 from unittest.mock import patch
 
 
-@fixture
-def no_wifi_handling():
-    with patch('just_start._just_start.manage_wifi'):
-        yield
+@fixture(scope='session', autouse=True)
+def mock_wifi_management():
+    patch('just_start._just_start.manage_wifi')
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,8 +1,9 @@
-from just_start import client
+from just_start import client, ClientError
+from just_start.client import refresh_tasks
+from pytest import raises
 
 
-# noinspection PyUnusedLocal
-def test_client_decorator_definition_and_calls(mocker, no_wifi_handling):
+def test_right_client_decoration():
     @client
     def write_status(_):
         pass
@@ -14,3 +15,28 @@ def test_client_decorator_definition_and_calls(mocker, no_wifi_handling):
     @client
     def on_tasks_refresh(_):
         pass
+
+
+def test_right_client_decoration_with_name_param():
+    @client('write_status')
+    def func():
+        pass
+
+
+def test_right_refresh_tasks_decoration():
+    @client
+    def on_tasks_refresh(_):
+        pass
+
+    @refresh_tasks
+    def refreshing_function():
+        pass
+
+    refreshing_function()
+
+
+def test_wrong_client_decoration():
+    with raises(ClientError):
+        @client
+        def wrong_name():
+            pass
