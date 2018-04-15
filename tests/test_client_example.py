4ac8a4dae0fc8dfbbbaf02d82abab408a1b7bce0
AliGhahraei <aligf94@gmail.com>
2018-04-15 16:30:49 -0500

    Change indentation

--- tests/test_client_example.py
+++ tests/test_client_example.py
@@ -13,29 +13,28 @@ def main_sysout(mocker, capsys, request):
 
 
 @mark.parametrize('main_sysout', (
-    ('a', 'Task description',),
-    ('c', '1',),
-    ('d', '1',),
-    ('m', 'task data', '1',),
-    ('!', 'task',),
-    ('l', 'w',),
-    ('l', 'h',),
-    ('!', 'command',),
-    ('h',),
-    ('p',),
-    ('p', 'p',),
-    ('s',),
-    ('r',),
-    ('t',),
-    ('y',),
+        ('a', 'Task description',),
+        ('c', '1',),
+        ('d', '1',),
+        ('m', 'task data', '1',),
+        ('!', 'task',),
+        ('l', 'w',),
+        ('l', 'h',),
+        ('!', 'command',),
+        ('h',),
+        ('p',),
+        ('p', 'p',),
+        ('s',),
+        ('t',),
+        ('y',),
 ), indirect=True)
 def test_right_action(main_sysout):
     assert INVALID_ACTION_KEY not in main_sysout
 
 
 @mark.parametrize('main_sysout', (
-    ('w',),
-    ('x',),
+        ('w',),
+        ('x',),
 ), indirect=True)
 def test_wrong_action(main_sysout):
     assert INVALID_ACTION_KEY in main_sysout
