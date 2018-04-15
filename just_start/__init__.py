aed8053ce79971e10c2ec319bad86c4436afb948
AliGhahraei <aligf94@gmail.com>
2018-04-15 12:12:08 -0500

    Add PomodoroError to public interface

--- just_start/__init__.py
+++ just_start/__init__.py
@@ -5,7 +5,7 @@ from ._just_start import (
 from .client import client_decorator as client, ClientError
 from .config_reader import ConfigError, get_client_config
 from .log import logger
-from .pomodoro import PromptSkippedPhases
+from .pomodoro import PromptSkippedPhases, PomodoroError
 from .utils import (
     JustStartError, TaskWarriorError, ActionError, UserInputError,
 )
@@ -15,6 +15,6 @@ __all__ = [
     'client', 'UNARY_ACTION_KEYS', 'initial_refresh_and_sync', 'Action',
     'NULLARY_ACTION_KEYS', 'ActionError', 'JustStartError', 'TaskWarriorError',
     'UserInputError', 'logger', 'ConfigError', 'PromptSkippedPhases',
-    'quit_just_start', 'UNARY_ACTIONS', 'get_client_config',
+    'quit_just_start', 'UNARY_ACTIONS', 'get_client_config', 'PomodoroError',
     'ClientError',
 ]
