e136d66518b1304fd4267c16a182b4c8648fa947
AliGhahraei <aligf94@gmail.com>
2018-04-15 01:14:36 -0500

    Use RunMock object for mocking subprocess' run

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
 from pytest import fixture
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 
 @fixture(scope='session', autouse=True)
 def mock_subprocess_run():
-    patch('just_start.utils.run')
-    patch('just_start.pomodoro.run')
+    class RunMock(MagicMock):
+        returncode = 0
+
+    with patch('just_start.utils.run', lambda *_, **__: RunMock()), \
+            patch('just_start.pomodoro.run', lambda *_, **__: RunMock()):
+        yield
