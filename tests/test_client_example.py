4147648bf32141a110796a3cdbd36564470f91c2
AliGhahraei <aligf94@gmail.com>
2018-04-15 16:17:58 -0500

    Factor out parametrized fixture and add test

--- tests/test_client_example.py
+++ tests/test_client_example.py
@@ -1,44 +1,49 @@
-from pytest import fixture
+from pytest import mark, fixture
 
-from just_start.client_example import main
+from just_start.client_example import main as client_main
 from just_start.constants import INVALID_ACTION_KEY
 
 
 @fixture
-def user_mock(mocker):
-    def prompt_mock(*args):
-        mocker.patch('just_start.client_example.prompt',
-                     side_effect=[*args, 'q'])
-    return prompt_mock
+def main_sysout(mocker, capsys, request):
+    user_input = (*request.param, 'q')
+    mocker.patch('just_start.client_example.prompt', side_effect=user_input)
+    client_main()
+    return capsys.readouterr()[0]
 
 
-# noinspection PyShadowingNames
-def test_right_actions(user_mock, capsys):
-    user_mock(
-        'a', 'Task description',
-        'c', '1',
-        'd', '1',
-        'm', 'task data', '1',
-        '!', 'task',
-        'l', 'w',
-        'l', 'h',
-        '!', 'command',
-        'h',
-        'p',
-        'p',
-        's',
-        'r',
-        't',
-        'y',
-    )
-    main()
-    out, _ = capsys.readouterr()
-    assert INVALID_ACTION_KEY not in out
+@mark.parametrize('main_sysout', (
+    ('a', 'Task description',),
+    ('c', '1',),
+    ('d', '1',),
+    ('m', 'task data', '1',),
+    ('!', 'task',),
+    ('l', 'w',),
+    ('l', 'h',),
+    ('!', 'command',),
+    ('h',),
+    ('p',),
+    ('p', 'p',),
+    ('s', '1',),
+    ('r',),
+    ('t',),
+    ('y',),
+), indirect=True)
+def test_right_action(main_sysout):
+    assert INVALID_ACTION_KEY not in main_sysout
 
 
-# noinspection PyShadowingNames
-def test_wrong_key(user_mock, capsys):
-    user_mock('z')
-    main()
-    out, _ = capsys.readouterr()
-    assert INVALID_ACTION_KEY in out
+@mark.parametrize('main_sysout', (
+    ('w',),
+    ('x',),
+), indirect=True)
+def test_wrong_action(main_sysout):
+    assert INVALID_ACTION_KEY in main_sysout
+
+
+def test_keyboard_interrupt(mocker):
+    def raise_keyboard_interrupt(*_, **__):
+        raise KeyboardInterrupt
+
+    mocker.patch('just_start.client_example.prompt', raise_keyboard_interrupt)
+    client_main()
