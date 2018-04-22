39598a6ba93f6f8347712b45a4dc93c9c66a78b8
Ali Ghahraei <aligf94@gmail.com>
2018-04-22 06:27:19 -0500

    Beautify code

--- just_start/_log.py
+++ just_start/_log.py
@@ -4,11 +4,8 @@ from os import makedirs
 from .constants import LOG_PATH, LOCAL_DIR
 
 log = getLogger('just_start')
-try:
-    _file_handler = FileHandler(LOG_PATH)
-except FileNotFoundError:
-    makedirs(LOCAL_DIR)
-    _file_handler = FileHandler(LOG_PATH)
 
-log.addHandler(_file_handler)
+makedirs(LOCAL_DIR, exist_ok=True)
+_file_handler = FileHandler(LOG_PATH)
 _file_handler.setFormatter(Formatter('%(asctime)s\n%(message)s'))
+log.addHandler(_file_handler)
