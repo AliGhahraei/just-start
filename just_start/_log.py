3bd8c29e638fc4a65042c42b5bdf541badc64085
Ali Ghahraei <aligf94@gmail.com>
2018-05-10 22:33:37 -0500

    Enrich logging format

--- just_start/_log.py
+++ just_start/_log.py
@@ -8,5 +8,6 @@ makedirs(LOCAL_DIR, exist_ok=True)
 log = getLogger('just_start')
 
 _file_handler = FileHandler(LOG_PATH)
-_file_handler.setFormatter(Formatter('%(asctime)s\n%(message)s'))
+_file_handler.setFormatter(Formatter('%(asctime)s| %(levelname)s| %(module)s@%(lineno)d'
+                                     '\n%(message)s'))
 log.addHandler(_file_handler)
