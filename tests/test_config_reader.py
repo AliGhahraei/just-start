79d07576383dd6ca29613887f1b149f5e6eb6a80
Ali Ghahraei <aligf94@gmail.com>
2018-10-11 22:05:10 -0500

    Remove redundant test

--- tests/test_config_reader.py
+++ tests/test_config_reader.py
@@ -1,11 +1,7 @@
 from just_start.config_reader import get_general_config, get_client_config
 
 
-def test_config_works_with_defaults():
-    assert get_general_config()
-
-
-def test_missing_config_file(mocker):
+def test_get_config_without_file(mocker):
     with mocker.patch('just_start.config_reader.load', side_effect=FileNotFoundError()):
         assert get_general_config()
 
